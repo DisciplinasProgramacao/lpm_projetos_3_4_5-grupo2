117b28a5a6b42da9cff926d4b1009e38b001801a
miguelmfc <martinsmiguelc@gmail.com>
2023-04-19 09:39:04 -0300

    Criar classe PlataformaStreaming

---
+++ codigo/src/PlataformaStreaming.java
@@ -0,0 +1,58 @@
+/*package src;
+
+import java.util.List;
+
+public class PlataformaStreaming {
+    private String nome;
+    private Hash<Serie> series;
+    private hash<Clientes> clientes;
+    private Cliente clienteAtual;
+    
+    public PlataformaStreaming(String nome) {
+        this.nome = nome;
+        this.series = new hash<Serie>();
+        this.clientes = new hash<Clientes>();
+        this.clienteAtual = null;
+    }
+    
+    public void login(Cliente cliente) {
+        // TODO: implementar
+    }
+    
+    public void adicionarSerie(Serie serie) {
+        // TODO: implementar
+    }
+    
+    public void adicionarCliente(Cliente cliente) {
+        // TODO: implementar
+    }
+    
+    public List<Serie> filtrarPorGenero(String genero) {
+        // TODO: implementar
+        return null;
+    }
+    
+    public List<Serie> filtrarPorIdioma(String idioma) {
+        // TODO: implementar
+        return null;
+    }
+    
+    public List<Serie> filtrarPorQntEps(int qntEps) {
+        // TODO: implementar
+        return null;
+    }
+    
+    public void registrarAudiencia(Serie serie) {
+        // TODO: implementar
+    }
+    
+    public void logoff() {
+        // TODO: implementar
+    }
+    
+    public Serie buscarSerie(String nomeSerie) {
+        // TODO: implementar
+        return null;
+    }
+}
+*/
