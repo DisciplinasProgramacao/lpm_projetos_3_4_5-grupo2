1c9c5de1226aa4e8d908ed00d86f7479158e57ef
Roberto Lafetá Mendonça Gomes <robertolmg1997@gmail.com>
2023-05-03 23:03:07 -0300

    Commit testCliente

--- src/Teste/ClienteTeste.java
+++ src/Teste/ClienteTeste.java
@@ -1,6 +1,8 @@
 package Teste;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.Test;
@@ -11,14 +13,33 @@ import codigo.Cliente;
 
 public class ClienteTeste {
     
-    Serie serie = new Serie("Game of Trhones", "Aventura", "Ingles", 100, 10000);
-    Cliente cliente = new Cliente("Roberto", "robertolmg", "123456");
-    
 
-    @Test
-    public void adicionarNaListaTeste(){
-        cliente.adicionarNaLista(serie);
-        assertEquals(cliente.getListaParaVer(), serie);
+    Serie serie;
+    Cliente cliente;
+
+	@BeforeEach
+	void init() {
+        serie = new Serie("Game of Trhones", "Aventura", "Ingles", 100, 10000);
+        cliente = new Cliente("Roberto", "robertolmg", "123456");
     }
 
+
+    @Test
+    public void testAdicionarNaLista(){
+        cliente.adicionarNaLista(serie);
+        assertEquals(1, cliente.getListaParaVer().size());
+    }
+
+    @Test
+    public void testRetirarDaLista(){
+        cliente.adicionarNaLista(serie);
+        cliente.retirarDaLista("Game of Trhones");
+        assertEquals(0, cliente.getListaParaVer().size());
+    }
+
+    @Test
+    public void testRegistrarNaAudiencia(){
+        cliente.registrarAudiencia(serie);
+        assertEquals(1, cliente.getListaJaVistas().size());
+    }
 }
