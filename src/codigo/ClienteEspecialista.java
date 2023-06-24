5bebec7d5712c3b92cb3e9d82a6c6fa6fa90de52
Lucas Figueira <lucasfigueira12@gmail.com>
2023-06-24 16:59:21 -0300

    Update Projeto 5 - Tipo Cliente

---
+++ src/codigo/ClienteEspecialista.java
@@ -0,0 +1,8 @@
+package codigo;
+
+public class ClienteEspecialista extends Cliente implements ITipoCliente {
+    @Override
+    public void avaliarMida() {
+
+    }
+}
---
+++ src/codigo/ClienteNormal.java
@@ -0,0 +1,8 @@
+package codigo;
+
+public class ClienteNormal extends Cliente implements ITipoCliente {
+    @Override
+    public void avaliarMida() {
+
+    }
+}
